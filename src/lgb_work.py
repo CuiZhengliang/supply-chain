4f327b369f28e000ccf04446d69be3875d040a9d
cauchy guo <cauchyguo@mail.com>
2018-12-26 23:23:33 +0800

    添加了一键运行代码
    Signed-off-by: cauchy guo <cauchyguo@mail.com>

--- src/lgb_work.py
+++ src/lgb_work.py
@@ -27,19 +27,19 @@ params = {
 
 if __name__ == '__main__':
     datas = list()
-    target = pd.read_csv(r'E:\PycharmProjects\Task2Plus\dataset\submit_example.csv')
-    relation = pd.read_csv(r'E:\PycharmProjects\Task2Plus\dataset\goods_sku_relation.csv')
+    target = pd.read_csv(r'E:\PycharmProjects\supply-chain\dataset\submit_example.csv')
+    relation = pd.read_csv(r'E:\PycharmProjects\supply-chain\dataset\goods_sku_relation.csv')
     target = pd.merge(target[['sku_id']], relation, on='sku_id')
     goods_list = target.goods_id.unique()
     scaler = MinMaxScaler()
 
     for i in range(6):
-        trainPath = r'E:\PycharmProjects\Task2Plus\TrainSets\trainWithLabel' + str(i + 1) + '.csv'
+        trainPath = r'E:\PycharmProjects\supply-chain\TrainSets\trainWithLabel' + str(i + 1) + '.csv'
         traindata = pd.read_csv(trainPath)
         datas.append(traindata[traindata.goods_id.isin(goods_list)])
 
     dataset = pd.concat(datas, axis=0)
-    test = pd.read_csv(r'E:\PycharmProjects\Task2Plus\TestSets\TestPlus.csv')
+    test = pd.read_csv(r'E:\PycharmProjects\supply-chain\TestSets\TestPlus.csv')
     # test = pd.merge(test,target,on='sku_id')
 
     # for week1
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     lgb_train = lgb.Dataset(X_train, label=y_train)
     lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
     gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=lgb_eval, early_stopping_rounds=100)
-    gbm.save_model(r'E:\PycharmProjects\Task2Plus\models\model1.txt')
+    gbm.save_model(r'E:\PycharmProjects\supply-chain\models\model1.txt')
     y_pred = gbm.predict(test[test.columns[2:]], num_iteration=gbm.best_iteration)
     # y_pred = y_pred.astype('int')
     test['week1'] = pd.Series(y_pred)
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     lgb_train = lgb.Dataset(X_train, label=y_train)
     lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
     gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=lgb_eval, early_stopping_rounds=100)
-    gbm.save_model(r'E:\PycharmProjects\Task2Plus\models\model2.txt')
+    gbm.save_model(r'E:\PycharmProjects\supply-chain\models\model2.txt')
     y_pred = gbm.predict(test[test.columns[2:]], num_iteration=gbm.best_iteration)
     # y_pred = y_pred.astype('int')
     test['week2'] = pd.Series(y_pred)
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     lgb_train = lgb.Dataset(X_train, label=y_train)
     lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
     gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=lgb_eval, early_stopping_rounds=100)
-    gbm.save_model(r'E:\PycharmProjects\Task2Plus\models\model3.txt')
+    gbm.save_model(r'E:\PycharmProjects\supply-chain\models\model3.txt')
     y_pred = gbm.predict(test[test.columns[2:]], num_iteration=gbm.best_iteration)
     # y_pred = y_pred.astype('int')
     test['week3'] = pd.Series(y_pred)
@@ -89,7 +89,7 @@ if __name__ == '__main__':
     lgb_train = lgb.Dataset(X_train, label=y_train)
     lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
     gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=lgb_eval, early_stopping_rounds=100)
-    gbm.save_model(r'E:\PycharmProjects\Task2Plus\models\model4.txt')
+    gbm.save_model(r'E:\PycharmProjects\supply-chain\models\model4.txt')
     y_pred = gbm.predict(test[test.columns[2:]], num_iteration=gbm.best_iteration)
     # y_pred = y_pred.astype('int')
     test['week4'] = pd.Series(y_pred)
@@ -102,10 +102,10 @@ if __name__ == '__main__':
     lgb_train = lgb.Dataset(X_train, label=y_train)
     lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
     gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=lgb_eval, early_stopping_rounds=100)
-    gbm.save_model(r'E:\PycharmProjects\Task2Plus\models\model5.txt')
+    gbm.save_model(r'E:\PycharmProjects\supply-chain\models\model5.txt')
     y_pred = gbm.predict(test[test.columns[2:]], num_iteration=gbm.best_iteration)
     # y_pred = y_pred.astype('int')
     test['week5'] = pd.Series(y_pred)
 
     test = test[['sku_id', 'week1', 'week2', 'week3', 'week4', 'week5']]
-    test.to_csv(r'E:\PycharmProjects\Task2Plus\output\lgb_reault.csv', index=False)
+    test.to_csv(r'E:\PycharmProjects\supply-chain\output\lgb_reault.csv', index=False)
--- src/run_all.py
+++ src/run_all.py
@@ -7,3 +7,14 @@
 @time: 2018/12/26 0:27
 """
 import os
+
+print("数据预处理中")
+os.system("python src/sedwork.py")
+print("划窗中")
+os.system("python src/data_split.py")
+print("打标中")
+os.system("python src/label_biu.py")
+print("特征提取中")
+os.system("python src/feature_extract.py")
+print("lgb模型处理中")
+os.system("python src/lgb_work.py")
