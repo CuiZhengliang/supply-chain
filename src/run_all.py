ecb32eb7a92002e26f37a4e7ede6ad2296fac1d2
cauchy guo <cauchyguo@mail.com>
2018-12-26 23:25:36 +0800

    大改
    Signed-off-by: cauchy guo <cauchyguo@mail.com>

--- src/run_all.py
+++ src/run_all.py
@@ -7,14 +7,25 @@
 @time: 2018/12/26 0:27
 """
 import os
+import datetime
+import time
 
-print("数据预处理中")
+start_time = datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), '%Y-%m-%d %H:%M:%S')
+print('start time :', start_time)
+
+print("数据预处理中……")
 os.system("python src/sedwork.py")
-print("划窗中")
+print("划窗中……")
 os.system("python src/data_split.py")
-print("打标中")
+print("打标中……")
 os.system("python src/label_biu.py")
-print("特征提取中")
+print("特征提取中……")
 os.system("python src/feature_extract.py")
-print("lgb模型处理中")
+print("lgb模型处理中……")
 os.system("python src/lgb_work.py")
+
+end_time = datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), '%Y-%m-%d %H:%M:%S')
+print('end time :', end_time)
+
+run_time = end_time - start_time
+print('run time :', run_time)
